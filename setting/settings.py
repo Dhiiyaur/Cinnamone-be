1054005ad7d607a3d5b0d6728cc73d34a742d261
Dhiiyaur <dhiyarifqi@gmail.com>
2021-02-21 21:58:29 +0700

    change setting

--- setting/settings.py
+++ setting/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = 'a+_=r#x^-4&gt(x^*jfu!nl-vp@_5_yo8m-p+*ds-vk+ukbgy9'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['be-cinnamone.herokuapp.com', '127.0.0.1']
 
 
 # Application definition
