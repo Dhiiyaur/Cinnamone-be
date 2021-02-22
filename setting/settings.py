1df882874cedce08b8af32b6309951078e792804
Dhiiyaur <dhiyarifqi@gmail.com>
2021-02-22 13:07:08 +0700

    a

--- setting/settings.py
+++ setting/settings.py
@@ -158,7 +158,7 @@ STATICFILES_DIRS = [
 
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static-cdn')
-MEDIA_URL = '/static-cdn/'	
+MEDIA_URL = '/media/'	
 MEDIA_ROOT = BASE_DIR
 
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
