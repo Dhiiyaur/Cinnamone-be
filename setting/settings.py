756dd18029a5d46d3f30e7acee93fbdd36028095
Dhiiyaur <dhiyarifqi@gmail.com>
2021-02-22 12:09:44 +0700

    change settings

--- setting/settings.py
+++ setting/settings.py
@@ -182,7 +182,7 @@ EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_USE_TLS = True
 EMAIL_PORT = 587
 EMAIL_HOST_USER = "projectkaho01@gmail.com"
-EMAIL_HOST_PASSWORD = "dhiyaulhaq"
+EMAIL_HOST_PASSWORD = "xibwuajwfzoopixr"
 
 
 # user account
